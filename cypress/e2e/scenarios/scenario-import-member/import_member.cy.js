84a3f5d92c2761904c9507d194110f980303d045
Liceth Yaneth <l.rozoq@uniandes.edu.co>
2023-05-07 12:52:55 -0500

    Import Member: Fix scenario

--- cypress/e2e/scenarios/scenario-import-member/import_member.cy.js
+++ cypress/e2e/scenarios/scenario-import-member/import_member.cy.js
@@ -1,28 +1,38 @@
 /// <reference types="cypress" />
 
-context( "Import Member", () => {    
-    it( "import member", () => {
-        // Given
-        cy.visit("http://localhost:2368/ghost");
+describe("Import members with CSV file", () => {
+    context("Given: Access web site", () => {
+        beforeEach( () => {
+            cy.visit("http://localhost:2369/ghost");
+        });
 
-        // When
-        cy.get("#identification").type("lrozoq@gmail.com");
-        cy.get("#password").type("PassTest3");
-        cy.get("button").contains(/Sign/).click({ force: true });
-        cy.get("a").contains(/Members/).click({ force: true });
-        if( !cy.get("table").should("exist") ) {
-            cy.contains(/CSV/).click();
-            importCSV("cypress/fixtures/member-import-template.csv");
-        } else {
-            cy.get("button[data-test-button='members-actions']").click();
-            cy.get("a[data-test-link='import-csv']").click();
-            importCSV("cypress/fixtures/member-import-template-v2.csv");
-        }
-
-        // Then
-        cy.get("table").should("exist")
-    })
-})
+        context("When: Validate whether the members have been saved correctly through a CSV file", () => {
+            beforeEach( () => {
+                cy.get("#identification").type("lrozoq@gmail.com");
+                cy.get("#password").type("I*%^sC!7B65Z");
+                cy.get("button").contains(/Sign/).click({ force: true });
+                cy.get("a").contains(/Members/).click({ force: true });
+                const existTableMember = cy.get("body").then( $body => {
+                    if( $body.find("table").length) {
+                        return true;
+                    }
+                    return false;
+                });
+                if( !existTableMember ) {
+                    cy.contains(/CSV/).click();
+                    importCSV("cypress/fixtures/member-import-template.csv");
+                } else {
+                    cy.get("button[data-test-button='members-actions']").click();
+                    cy.get("a[data-test-link='import-csv']").click();
+                    importCSV("cypress/fixtures/member-import-template-v2.csv");
+                }
+            });
+            it("Then: A table should exist showing the created members.", () => {
+                cy.get("table").should("exist")
+            })
+        });
+    });
+});
 
 function importCSV( pathcsv ) {
     cy.get("input[type=file]").eq(0).selectFile(pathcsv, { force: true })
