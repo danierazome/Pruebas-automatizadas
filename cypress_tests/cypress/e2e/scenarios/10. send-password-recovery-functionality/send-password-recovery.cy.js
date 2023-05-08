8c773e0e655e23ebbc2fcc789bf8c41a0cedbe9f
Liceth Yaneth <l.rozoq@uniandes.edu.co>
2023-05-07 23:12:27 -0500

    Scenario: Send recovery message error

---
+++ cypress_tests/cypress/e2e/scenarios/10. send-password-recovery-functionality/send-password-recovery.cy.js
@@ -0,0 +1,18 @@
+import LoginPage from "../../../support/pageobjects/LoginPage";
+
+describe("Verify that a password recovery message is not sent when the recovery process is initiated but not completed", () => {
+    context("Given: Access web site", () => {
+        beforeEach(() => {
+            LoginPage.visitLoginPage();
+        });
+        context("When: Enter the email into the input field and click on the 'forgot' button ", () => {
+            beforeEach(() => {
+                LoginPage.fillEmailLogin();
+                cy.get("button").contains("Forgot").click();
+            });
+            it("Then: displays an error message if email delivery fails ' ", () => {
+                cy.contains("Failed to send email. Reason: Sending failed.").should("exist");
+            });
+        });
+    });
+});
