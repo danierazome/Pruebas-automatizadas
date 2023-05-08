2e06e8f2d6090a96a0b5ca714b421a03bc54f361
Santiago Sierra Rassa <s.sierrar2@uniandes.edu.co>
2023-05-07 23:23:22 -0500

    FIX: se sube ub fix para el escenario de pruebas 08

--- kraken_tests/kraken_project/features/sc08.f
+++ kraken_tests/kraken_project/features/sc08.f
@@ -6,7 +6,7 @@ Scenario: Buscar post creado previamente por nombre de tag
   And I wait for 5 seconds
   When I log in
   And I wait for 5 seconds
-  And I navigate to page "http://localhost:2368/ghost/#/posts?tag=tag-numero-3"
+  And I navigate to page "http://localhost:2368/ghost/#/posts?tag=tag-numero-1"
   And I wait for 2 seconds
   Then I click a button "Click in the post found by tag" 'a[class="ember-view permalink gh-list-data gh-post-list-title"]'
   And I wait for 2 seconds
