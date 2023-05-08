6f16d6c3b04a5d1de7c0bbaf41e4bb87b98157d8
Juan Montenegro <jj.montenegro@uniandes.edu.co>
2023-05-07 19:08:39 -0500

    update const base URL in each scenary

--- cypress_tests/cypress/e2e/scenarios/2. create-post-functionality/createPost_scenario__2.cy.js
+++ cypress_tests/cypress/e2e/scenarios/2. create-post-functionality/createPost_scenario__2.cy.js
@@ -1,19 +1,20 @@
 describe("Create a post scenario 2 ", () => {
+  const baseURL = Cypress.env("baseURL");
   it("Visits Ghost Web application", () => {
     cy.viewport(1280, 720);
     // LogIn page
-    cy.visit("${baseURL}/ghost/#/signin");
+    cy.visit(`${baseURL}/ghost/#/signin`);
     // LogingIn
     cy.get("#ember8").type("correoDePrueba@gmail.com", { force: true });
     cy.get("#ember10").type("@1234567890*", { force: true });
     cy.wait(1000);
     cy.get("#ember12").click();
-    cy.visit("${baseURL}/ghost/#/site");
+    cy.visit(`${baseURL}/ghost/#/site`);
     // expand Posts Section
     cy.wait(2000);
     cy.get("#ember28").click();
     cy.wait(2000);
-    cy.get('a[href="#/editor/post/"]').visit("${baseURL}/ghost/#/editor/post/");
+    cy.get(`a[href="#/editor/post/"]`).visit(`${baseURL}/ghost/#/editor/post/`);
     cy.wait(2000);
     cy.get("textarea[tabindex=1]").type("Segundo Post de prueba con cypress");
     cy.wait(1000);
@@ -34,8 +35,8 @@ describe("Create a post scenario 2 ", () => {
     cy.wait(1000);
     cy.get("button").contains("span", "Publish").click();
     cy.wait(1000);
-    cy.get('a[href="#/posts/"]').visit("${baseURL}/ghost/#/posts");
+    cy.get(`a[href="#/posts/"]`).visit(`${baseURL}/ghost/#/posts`);
     cy.wait(1000);
-    cy.visit("${baseURL}/ghost/#/site");
+    cy.visit(`${baseURL}/ghost/#/site`);
   });
 });
