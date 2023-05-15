cbcf8b2bac6a6717a66c8c662c4b08ffec1dbf82
lrozoq <l.rozoq@uniandes.edu.co>
2023-05-15 00:14:24 -0500

    correcion nombre

--- cypress_tests/cypress/e2e/v5.47.1/scenarios/7.restorePassword_Functionality.cy.js
+++ cypress_tests/cypress/e2e/v5.47.1/scenarios/7.restorePassword_Functionality.cy.js
@@ -1,19 +1,13 @@
 import LoginPage from "../pages/LoginPage";
 
 describe("Verify that a password recovery message is not sent when the recovery process is initiated but not completed", () => {
-    context("Given: Access web site", () => {
         beforeEach(() => {
             LoginPage.visitLoginPage();
+            LoginPage.fillEmailLogin();
         });
-        context("When: Enter the email into the input field and click on the 'forgot' button ", () => {
-            beforeEach(() => {
-                LoginPage.fillEmailLogin();
+        it("Then: displays an error message if email delivery fails ' ", () => {
                 cy.get("button").contains("Forgot").click();
-            });
-            it("Then: displays an error message if email delivery fails ' ", () => {
                 cy.contains("Failed to send email. Reason: Sending failed.").should("exist");
                 cy.screenshot(`v5-${Cypress.currentTest.titlePath.join("/")}/step`);
-            });
         });
-    });
 });
